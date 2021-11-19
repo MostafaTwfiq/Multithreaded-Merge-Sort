c6befeb7307e3689e263561dd6dce3e296880b41
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2021-11-19 20:05:01 +0200

    First Commit.

---
+++ cmake-build-debug/CMakeFiles/MergeSortMultithreaded.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MergeSortMultithreaded.dir/main.c.obj"
+  "MergeSortMultithreaded.exe"
+  "MergeSortMultithreaded.exe.manifest"
+  "MergeSortMultithreaded.pdb"
+  "libMergeSortMultithreaded.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MergeSortMultithreaded.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "string.h"
+#include <pthread.h>
+
+typedef struct Param {
+    int *arr, s, e;
+} Param;
+
+void mergeSort(void *p) {
+    Param *param = (Param *) p;
+    int *arr = param->arr;
+    int s = param->s, e = param->e;
+
+    if (s >= e)
+        pthread_exit(NULL);
+
+    int fHalfEnd = (e - s) / 2  + s;
+
+    Param newParam1 = {arr, s, fHalfEnd};
+    Param newParam2 = {arr, fHalfEnd + 1, e};
+    int th1ID, th2ID;
+    pthread_create(&th1ID, NULL, mergeSort, &newParam1);
+    pthread_create(&th2ID, NULL, mergeSort, &newParam2);
+
+    if (pthread_join(th1ID, NULL))
+        fprintf(stderr, "Some thing went wrong.");
+    if (pthread_join(th2ID, NULL))
+        fprintf(stderr, "Some thing went wrong.");
+
+
+    int temp[e - s + 1], count = 0;
+    int *fP = arr + s, *sP = arr + fHalfEnd + 1;
+    while(fP <= arr + fHalfEnd && sP <= arr + e)
+        temp[count++] = *fP < *sP ? *fP++ : *sP++;
+    while (fP <= arr + fHalfEnd)
+        temp[count++] = *fP++;
+    while (sP <= arr + e)
+        temp[count++] = *sP++;
+
+    for (int i = 0; i < count; i++)
+        arr[s + i] = temp[i];
+
+    pthread_exit(NULL);
+}
+
+int main() {
+    char path[] = "C:\\Users\\mosta\\CLionProjects\\MergeSortMultithreaded\\input.txt";
+    int size, *arr;
+    FILE *f = fopen(path, "r");
+    if (!f) {
+        fprintf(stderr, "Can't open the input file.");
+        exit(-1);
+    }
+
+    FILE *ansFile = fopen("ans.txt", "w");
+    char sizeInput[100];
+    fgets(sizeInput, 100, f);
+    sizeInput[strlen(sizeInput) - 1] = '\0';
+    size = atoi(sizeInput);
+
+    arr = (int *) malloc(sizeof(int) * size);
+    int temp;
+    for (int i = 0; i < size; i++) {
+        fscanf(f, "%d", &temp);
+        arr[i] = temp;
+    }
+
+    if (size == 1)
+        goto Finish;
+
+    Param param = {arr, 0, 10};
+    mergeSort(&param);
+
+    Finish:
+    if (!ansFile) {
+        fprintf(stderr, "Can't create the answer file.");
+        exit(-1);
+    }
+
+    fprintf(ansFile, "Sorted array: ");
+    for (int i = 0; i < size; i++)
+        fprintf(ansFile, "%d%c", arr[i], i == size - 1 ? '\0' : ' ');
+
+    free(arr);
+    fclose(f);
+    fclose(ansFile);
+    return 0;
+}
